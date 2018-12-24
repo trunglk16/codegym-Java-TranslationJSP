ecacbdcc71c70b2c3c907dded560730b7849dd34
trunglk16 <trungnguyenlk16@gmail.com>
2018-12-24 10:35:31 +0700

    First

---
+++ src/main/java/dictionary/dictionaryServlet.java
@@ -0,0 +1,35 @@
+package dictionary;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class dictionaryServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        Map<String, String> dictionary = new HashMap<>();
+        dictionary.put("hello", "Xin chào");
+        dictionary.put("how", "Thế nào");
+        dictionary.put("book", "Quyển vở");
+        dictionary.put("computer", "Máy tính");
+        String search = request.getParameter("txtSearch");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+
+        String result = dictionary.get(search);
+        if(result != null){
+            writer.println("Word: " + search);
+            writer.println("Result: " + result);
+        } else {
+            writer.println("Not found");
+        }
+
+        writer.println("</html>");
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
